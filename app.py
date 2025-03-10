5cbd354d4df36abdea380abcb2e6ca7a97085ad9
StepanSokolovsky <stepan.sokol@list.ru>
2025-03-10 13:38:02 +0300

    new project

--- app.py
+++ app.py
@@ -1,69 +1,58 @@
-from fastapi import FastAPI, UploadFile, BackgroundTasks
 from contextlib import asynccontextmanager
-
+import pickle
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
 
+app = FastAPI()
 
-PATH = 'https://github.com/evgpat/datasets/raw/refs/heads/main/penguins.csv'
-ml_models = {}
+class Item(BaseModel):
+    name: str
+    year: int
+    selling_price: int
+    km_driven: int
+    fuel: str
+    seller_type: str
+    transmission: str
+    owner: str
+    mileage: str
+    engine: str
+    max_power: str
+    torque: str
+    seats: float
 
-def penguins_pipeline():
-    data = pd.read_csv(PATH)
-    data = data.dropna()
-
-    le = preprocessing.LabelEncoder()
-    X = data[["bill_length_mm", "flipper_length_mm"]]
-    le.fit(data["species"])
-    y = le.transform(data["species"])
-    X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
-    clf = Pipeline(
-        steps=[("scaler", StandardScaler()), ("knn", KNeighborsClassifier(n_neighbors=11))]
-    )
-    clf.set_params().fit(X_train, y_train)
-
-    return clf, le
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    clf, le = penguins_pipeline()
-
-    ml_models["clf"] = clf
-    ml_models["le"] = le
-
-    yield
-
-    ml_models.clear()
-
-app = FastAPI(lifespan=lifespan)
-
+class Items(BaseModel):
+    objects: List[Item]
 
 @app.get("/")
 async def root():
     return {
-        "Name": "Penguins Prediction",
-        "description": "This is a penguins prediction model based on the bill length and flipper length of the bird.",
+        "Name": "Cars Prediction",
+        "description": "This is a cars' price prediction model based on their typical features.",
     }
 
-@app.get("/predict/")
-async def predict(bill_length_mm: float = 0.0, flipper_length_mm: float = 0.0):
-    param = {
-                "bill_length_mm": bill_length_mm,
-                "flipper_length_mm": flipper_length_mm
-            }
-    if bill_length_mm <=0.0 or flipper_length_mm <=0.0:
-        return {
-            "parameters": param,
-            "error message": "Invalid input values",
-        }
-    else:
-        result = ml_models["clf"].predict([[bill_length_mm, flipper_length_mm]])
-        return {
-            "parameters": param,
-            "result": ml_models["le"].inverse_transform(result)[0],
-        }
+def medinc_regressor(x: dict) -> dict:
+    with open("model.pkl", 'rb') as model_file:
+        loaded_model = pickle.load(model_file)
+    x_df = pd.DataFrame(x, index=[0])
+    res = loaded_model.predict(x_df)[0]
+    return {"prediction": res}
+
+ml_models = {}
+
+@asynccontextmanager
+async def ml_lifespan_manager(app: FastAPI):
+    ml_models["medinc_regressor"] = medinc_regressor
+    yield
+    ml_models.clear()
+
+app = FastAPI(lifespan=ml_lifespan_manager)
+
+@app.post("/predict_item")
+async def predict(item: Item):
+    return ml_models["medinc_regressor"](item.model_dump())
+
+@app.post("/predict_items")
+def predict_items(items: List[Item]) -> List[float]:
+    return [ml_models["medinc_regressor"](item.model_dump()) for item in items]
